f98a17d98780b86aab8ed388cbd3b227b99d195d
Jeff R Lynch <jeffrlynch@gmail.com>
2018-11-01 12:55:45 -0400

    Recreated UnitTesting project since somehow got jacked up. WIll be adding more tests

---
+++ StampReaderTests/StringToolsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StampReader;
+
+namespace StampReaderTests
+{
+    [TestClass()]
+    public class StringToolsTests
+    {
+        [TestMethod()]
+        public void ConvertInputToMultiFormatTest()
+        {
+            //Test for handling single stamp specified even though multi selected
+            string myInput = "1345";
+            string myResult = StringTools.ConvertInputToMultiFormat(myInput);
+            Assert.AreEqual("('1345')", myResult);
+        }
+
+        [TestMethod()]
+        public void ConvertInputToMultiFormatTest1()
+        {
+            //Test for handling multi stamps specified
+            string myInput = "1345;4798;873;t3544";
+            string myResult = StringTools.ConvertInputToMultiFormat(myInput);
+            Assert.AreEqual("('1345','4798','873','t3544')", myResult);
+        }
+    }
+}
